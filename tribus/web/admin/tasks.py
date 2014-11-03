ed9c9c79a8210a0a55dcf7928c6446826de0426c
Jose Francisco Guerrero <jsfrncscg@gmail.com>
2014-11-03 15:31:03 -0430

    Comentando switches

--- tribus/web/admin/tasks.py
+++ tribus/web/admin/tasks.py
@@ -1,17 +1,17 @@
-from celery import task
-from tribus import BASEDIR
-from tribus.config.switches import SWITCHES_CONFIGURATION
-from fabric.api import local, cd
+# from celery import task
+# from tribus import BASEDIR
+# from tribus.config.switches import SWITCHES_CONFIGURATION
+# from fabric.api import local, cd
 
-@task
-def update_switches(*args):
-    POST = args[0]
-    for switch_name in SWITCHES_CONFIGURATION.keys():
-        if switch_name in POST:
-            print "SWITCH %s: %s" % (switch_name, POST.get(switch_name))
-            with cd('%s' % BASEDIR):
-                local('python manage.py switch %s on' % switch_name)
-        else:
-            print "SWITCH %s: off" % (switch_name)
-            with cd('%s' % BASEDIR):
-                local('python manage.py switch %s off' % switch_name)
+# @task
+# def update_switches(*args):
+#     POST = args[0]
+#     for switch_name in SWITCHES_CONFIGURATION.keys():
+#         if switch_name in POST:
+#             print "SWITCH %s: %s" % (switch_name, POST.get(switch_name))
+#             with cd('%s' % BASEDIR):
+#                 local('python manage.py switch %s on' % switch_name)
+#         else:
+#             print "SWITCH %s: off" % (switch_name)
+#             with cd('%s' % BASEDIR):
+#                 local('python manage.py switch %s off' % switch_name)
